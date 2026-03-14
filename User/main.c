96c13e8567db04d741dd9d333e37ffbb8ada1752
Jiacheng Zhang <1439395767@qq.com>
2026-03-14 23:22:06 +0800

    20260314小修改
    No particularly significant changes

--- User/main.c
+++ User/main.c
@@ -6,8 +6,6 @@ int main(void)
 {
 	OLED_Init();
 	
-	uint8_t Echo;
-	
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 	
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -24,19 +22,6 @@ int main(void)
 	
 	while(1)
 	{
-		GPIO_SetBits(GPIOA, GPIO_Pin_1);
-		Delay_us(20);
-		GPIO_ResetBits(GPIOA, GPIO_Pin_1);
-		Echo = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_2);
-		OLED_ShowString(2, 1, "Echo:");
-		OLED_ShowNum(2, 5, Echo, 1);
-		while (Echo == 1)
-		{
-			GPIO_ResetBits(GPIOA, GPIO_Pin_0);
-			Echo = 0;
-			break;
-		}
-		Delay_ms(200);
-		GPIO_SetBits(GPIOA, GPIO_Pin_0);
+		
 	}
 }
